5edc0a190066546d548155fca7bca01a77080dd4
Donald Berry <dberry.offical@gmail.com>
2021-04-10 15:28:14 -0500

    Exercise 2

--- app/views.py
+++ app/views.py
@@ -6,11 +6,35 @@ This file creates your application.
 """
 
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect
+import os
+from werkzeug.utils import secure_filename
 
 ###
 # Routing for your application.
 ###
+
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    form= UploadForm()
+    if method == "POST" and not form.validate_on_submit():
+        response = jsonify(success = False, errors = form_errors())
+        return response
+
+        photo = request.files['photo']
+        try:
+            photo_ = secure_filename(photo_)
+            photo.save(os.path.join(app.config['UPLOAD_FOLDER'], photo_filename))
+        except:
+            response = jsonify(sucess = False, error = ["Error while saving fiile"])
+            return response
+        
+    response = jsonify(success = True,
+    message = "File Upload Successful",
+    filename = photo_,
+    description = form.description.data )
+
+    return response
 
 
 # Please create all new routes and view functions above this route.
